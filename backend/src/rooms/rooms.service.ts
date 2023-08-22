8a2fcb8484b07801884452e9796db68a7dd1c2f7
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-08-22 13:52:05 -0300

    Update rooms.service.ts

--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { join } from 'path';
+import * as fs from 'fs';
+import { readParse } from '../utils/utils';
 
 @Injectable()
 export class RoomsService {
     private Rooms= join(__dirname, '../../data/Rooms.json')
     getAll(){
+        console.log(this.Rooms)
     return this.Rooms
     }
-    create(){
-
+    async create(Room: any){
+        try {
+            const newRoom = {...Room};
+            const data = readParse();
+            data.push(newRoom);
+            fs.writeFileSync(this.Rooms,
+            JSON.stringify(data, null, 2 ))
+            const Data = fs.readFileSync(this.Rooms, 'utf-8');
+            console.log (Data);
+          } catch (error) {
+            console.log(error);
+          }
     }
+    
+
+
 }
