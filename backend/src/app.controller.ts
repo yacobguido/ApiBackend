43c1861d6b4f7cb2c53a563b6cef7f0802a2e0eb
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-08-22 14:00:00 -0300

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 
 import { RoomsService } from './rooms/rooms.service';
@@ -8,8 +8,12 @@ export class RoomsController {
   constructor(private roomsService: RoomsService){}
   @Get()
   getAll(@Res() res: Response){
-    const Rooms = this.roomsService.getAll()
-    res.sendFile(Rooms)
+    res.sendFile(this.roomsService.getAll())
+  }
+  @Post()
+  create(@Body() Rooms: any){
+    this.roomsService.create(Rooms)
+    return {Message:'Room saved', Room: Rooms}
   }
 
 }
