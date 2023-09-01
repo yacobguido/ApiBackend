5657414671708583d1125402cd04899bd1538690
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-09-01 20:02:39 -0300

    Update utils.ts

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,9 +1,16 @@
-
-import *as fs from 'fs';
-import { join } from 'path';
+import * as fs from 'fs'
+import {join} from 'path'
 
 export function readParse(){
-    const Rooms= join(__dirname, '../../data/Rooms.json')
+    const Rooms = join(__dirname, '../../data/Rooms.json')
     const fileContent = fs.readFileSync(Rooms, 'utf-8');
+    console.log(fileContent);
     return JSON.parse(fileContent);
+}
+
+export function createID(){
+    const Rooms = readParse();
+    const lastRoom = Rooms[Rooms.length - 1];
+    const id = lastRoom.id + 1;
+    return id;
 }
