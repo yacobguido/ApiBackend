6d451c8cb944a73a9b18dbd43d5711d092c2a8ee
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-09-05 18:05:41 -0300

    Update rooms.service.ts

--- backend/src/rooms/rooms.service.ts
+++ backend/src/rooms/rooms.service.ts
@@ -12,45 +12,59 @@ export class RoomsService {
         throw new Error('Cannot get data')
     }
     }
-    getByNumberRoom(numberRoom: number) : {data?: any, success: boolean, message: string}{
-      try {
-          const data = readParse();
-          const index = data.findIndex((Room: { numberRoom: number; }) => Room.numberRoom === numberRoom);
-          if(index >= 0){
-              return {success: true, message: 'Room found', data: data[index]}
-          } else return {success: false, message: 'Room not found'}
-      } catch (error) {
-          throw new Error("Error getting room");
-      }
-  }
-  async create(Room: any){
+   create(Room: any): {success: boolean, message: string} {
     try {
         const newRoom = {id: createID(), ...Room};
         console.log(newRoom);
         const data = readParse();
         data.push(newRoom);
         fs.writeFileSync(this.Rooms, JSON.stringify(data, null, 2))
-        return {message: "Room created", data: newRoom, success: true}
+        return {message: `Room created id:${newRoom.id}`, success: true}
     } catch (error) {
         throw new Error("Created failed");
     }
   }
-  deleteByNumberRoom(numberRoom: string): {success: boolean, message: string}{
+  deleteByID(id: string): {success: boolean, message: string}{
     try {
         const data = readParse();
-        const roomFound = data.findIndex((Room: { numberRoom: number; }) => Room.numberRoom === Number(numberRoom))
+        const roomFound = data.findIndex((Room: { id: number; }) => Room.id === Number(id))
         if(roomFound >= 0){
             data.splice(roomFound, 1)
             fs.writeFileSync(this.Rooms, JSON.stringify(data, null, 2))
-            return {success: true, message: `Room with number room: ${numberRoom}, was deleted`}
+            return {success: true, message: `Room with id: ${id}, was deleted`}
         } else {
-            return {success: false, message: `Room with number room: ${numberRoom}, was not found`}
+            return {success: false, message: `Room with id: ${id}, was not found`}
         }
     } catch (error) {
         throw new Error(`Delete Failed`)
     }
-        }
+    }
     
-
+    updateByNumberRoom(numberRoom: number, body: any): {success: boolean, message: string, data?: any}{
+        try {
+            const data = readParse();
+            const index = data.findIndex((room : {numberRoom : number}) => room.numberRoom === numberRoom)
+            if(index >= 0){
+                const editedRoom = {...data[index], ...body, numberRoom}
+                data[index] = editedRoom
+                fs.writeFileSync(this.Rooms, JSON.stringify(data, null, 2))
+                return {success: true, message: `Room with number Room: ${numberRoom}, was edited`, data: editedRoom}
+            } return {success: false, message: `Room with number Room: ${numberRoom}, was not found`};
+        } catch (error) {
+                throw new Error(`Update Failed`)
+        }
+    }
+            
+    getByNumberRoom(numberRoom: number) : {data?: any, success: boolean, message: string}{
+        try {
+            const data = readParse();
+            const index = data.findIndex((room: { numberRoom: number; }) => room.numberRoom === numberRoom);
+            if(index >= 0){
+            return {success: true, message: 'Room found', data: data[index]}
+            } else return {success: false, message: 'Room not found'}
+        } catch (error) {
+            throw new Error("Error getting Room");
+        }
+    }
 
 }
