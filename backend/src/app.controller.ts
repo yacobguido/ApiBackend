c0c5ac2d99570ee8c8799d85d2f28cb797fcf3d8
yacobguido <110505095+yacobguido@users.noreply.github.com>
2023-09-15 11:27:28 -0300

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get, Res, Post, Delete, Put, Body, Param, HttpStatus, NotFoundException, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Res, Post, Delete, Put, Body, Param, HttpStatus, NotFoundException, BadRequestException, ValidationPipe, UsePipes} from '@nestjs/common';
 import { Response } from 'express';
 import {RoomsService} from './rooms/rooms.service';
+import { Room } from './rooms/rooms.interface';
+import { roomDto} from './rooms/room.dto';
+import { room_idDTO } from './rooms/room_id.dto';
+
 
 @Controller('Rooms')
 export class RoomsController {
@@ -8,68 +12,76 @@ export class RoomsController {
   constructor(private roomsService: RoomsService){}
 
   @Get()
-  getAll(@Res() res: Response ){
- try {
-  const serviceResponse = this.roomsService.getAll();
-  return res.status(HttpStatus.OK).send(serviceResponse)
- } catch (error) {
-  throw new NotFoundException('Data not found')
- }
+  async getAll(@Res() res: Response ): Promise <Response <room_idDTO>> {
+    try {
+      const serviceResponse = await this.roomsService.getAll();
+      return res.status(HttpStatus.OK).send(serviceResponse);
+    } catch (error) {
+      throw new NotFoundException('Data not found')
+    }
   }
 
-  @Get(':numberRoom')
-  getByNumberRoom(@Param('numberRoom') numberRoom: string, @Res() res: Response ){
-  try {
-    const parsedID = parseInt(numberRoom, 10);
-    const serviceResponse = this.roomsService.getByNumberRoom(parsedID);
-    if(serviceResponse.success){
-      return res.status(HttpStatus.OK).send(serviceResponse)
-    } else {
-      return res.status(HttpStatus.NOT_FOUND).send(serviceResponse)
+  @Get(':id')
+  async getByID(@Param('id') id: number, @Res() res: Response ): Promise<Response<Room>> {
+    try {
+      const serviceResponse = await this.roomsService.getByID(id);
+      if (serviceResponse) {
+        return res.status(HttpStatus.OK).send({message:`Room found`, Room:serviceResponse})
+      } else {
+        return res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ message: 'Room not found' });
+      }
+    } catch (error) {
+      throw new BadRequestException(`Cannot get Room with id ${id}`);
     }
-  } catch (error) {
-    throw new BadRequestException(`Cannot get room with number room ${numberRoom}`)
-  }
   }
 
   @Post()
-  create(@Body() Room: any, @Res() res: Response){
+  @UsePipes(new ValidationPipe({ transform: true}))
+  async create(@Body() Room: roomDto,  @Res() res:Response): Promise <any> {
     try {
-      const newRoom =this.roomsService.create(Room)
-      return res.status(HttpStatus.CREATED).send({...newRoom})
+      const serviceResponse = await this.roomsService.create(Room)
+      return res.status(HttpStatus.CREATED).send({message:`Room created`, Room:serviceResponse });
     } catch (error) {
       throw new BadRequestException('Room creation failed')
     }
   }
   @Delete(':id')
-  deleteByID(@Param('id') id: string, @Res() res:Response){
+  async deleteByID(@Param('id') id: number, @Res() res:Response): Promise <any> {
     try {
-      const serviceResponse = this.roomsService.deleteByID(id);
-      if(serviceResponse.success){
-        return res.status(HttpStatus.OK).send({...serviceResponse})
+      const serviceResponse = await this.roomsService.deleteByID(id);
+
+      if (serviceResponse) {
+        return res.status(HttpStatus.OK).send({message:`Room deleted`});
+      } else {
+        return res
+        .status(HttpStatus.NOT_FOUND)
+        .json({message:`Room not deleted`});
       }
-      else {
-        return res.status(HttpStatus.NOT_FOUND).send({...serviceResponse})
-      }
-      
     } catch (error) {
-      throw new NotFoundException('Delete failed')
+      throw new NotFoundException('Delete failed');
     }
   }
-  @Put(':numberRoom')
-  updateByNumberRoom(@Param('numberRoom') numberRoom: string, @Body() body: any, @Res() res: Response){
+  @Put(':id')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  async updateByID(@Param('id') id: number, @Body() body: roomDto, @Res() res: Response): Promise <any> {
     try {
-      const parsedNumberRoom = parseInt(numberRoom, 10);
-      const serviceResponse = this.roomsService.updateByNumberRoom(parsedNumberRoom, body);
-      if(serviceResponse.success){
-        return res.status(HttpStatus.OK).send({...serviceResponse, code: HttpStatus.OK})
-      } else {
-        return res.status(HttpStatus.NOT_FOUND).send({...serviceResponse, code: HttpStatus.NOT_FOUND})
+      const success = await this.roomsService.updateByID(id, body);
+
+      if(success) {
+        return res.status(HttpStatus.OK).send({
+          message:`Room edited`,
+          success
+        });
+      }else{
+        return res
+        .status(HttpStatus.NOT_FOUND)
+        .json({message:`Room not edited`});
       }
     } catch (error) {
-      throw new BadRequestException(`Update failed`)
+      throw new BadRequestException(`Update failed`);
     }
   }
 }
-
 
